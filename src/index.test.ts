09f5573cf91ed7fccdeab96c86427da59c60aacb
Muhun Kim <iam@muhun.kim>
2020-06-29 14:49:24 +0900

    Implement Lines Diff
    참고한 자료: https://alex.dzyoba.com/blog/writing-diff
    
    위 자료에서 Diff는 LIS와 반대의 요소가 필요하기 때문에 백트래킹으로 푸는 것을 권장하길래 그렇게 구현해봤습니다.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,12 @@
-test('Hello', () => {
-  expect(true).toBe(true);
+import { generateLinesDiff, DiffType } from '.';
+
+const original = ['abcde', 'fghijk'];
+const modefied = ['abcde', 'fghjk'];
+
+test('Lines Diff', () => {
+  expect(generateLinesDiff(original, modefied)).toEqual([
+    { type: DiffType.IDLE, content: original[0] },
+    { type: DiffType.DELETE, content: original[1] },
+    { type: DiffType.ADD, content: modefied[1] },
+  ]);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-console.log('Hello, world!');
+import { LCSLength } from './utils/lcs';
+
+export enum DiffType {
+  'IDLE',
+  'ADD',
+  'DELETE',
+}
+
+interface DiffLine {
+  type: DiffType;
+  content: string;
+}
+
+export function generateLinesDiff(original: string[], modified: string[]) {
+  const common = LCSLength<string[]>(original, modified);
+  const result: DiffLine[] = [];
+
+  function backtrack(originalIndex: number, modifiedIndex: number) {
+    if (originalIndex < 0 && modifiedIndex < 0) return;
+    if (originalIndex < 0) {
+      backtrack(originalIndex, modifiedIndex - 1);
+      result.push({ type: DiffType.ADD, content: modified[modifiedIndex] });
+      return;
+    }
+    if (modifiedIndex < 0) {
+      backtrack(originalIndex - 1, modifiedIndex);
+      result.push({ type: DiffType.DELETE, content: original[originalIndex] });
+      return;
+    }
+    if (original[originalIndex] === modified[modifiedIndex]) {
+      backtrack(originalIndex - 1, modifiedIndex - 1);
+      result.push({ type: DiffType.IDLE, content: original[originalIndex] });
+      return;
+    }
+    if (
+      common[originalIndex][modifiedIndex - 1] >=
+      common[originalIndex - 1][modifiedIndex]
+    ) {
+      backtrack(originalIndex, modifiedIndex - 1);
+      result.push({ type: DiffType.ADD, content: modified[modifiedIndex] });
+      return;
+    }
+    backtrack(originalIndex - 1, modifiedIndex);
+    result.push({ type: DiffType.DELETE, content: original[originalIndex] });
+  }
+  backtrack(original.length - 1, modified.length - 1);
+  return result;
+}
