26ca2421cf72afbca4290e6cf84bbac759c53a31
Muhun Kim <iam@muhun.kim>
2020-06-29 20:40:26 +0900

    Remove Reading out all LCS function
    백트래킹으로 diff를 탐색하는게 효율적이기 때문에 굳이 필요하지 않습니다.

--- src/utils/lcs.ts
+++ src/utils/lcs.ts
@@ -20,22 +20,3 @@ export function LCSLengths<T extends string | string[]>(first: T, second: T) {
   }
   return common;
 }
-
-export function LCS<T extends string | string[]>(first: T, second: T) {
-  function backtrack(firstIndex: number, secondIndex: number): string {
-    if (firstIndex === -1 || secondIndex === -1) return '';
-    if (first[firstIndex] == second[secondIndex])
-      return backtrack(firstIndex - 1, secondIndex - 1) + first[firstIndex];
-    if (
-      common[firstIndex][secondIndex - 1] >= common[firstIndex - 1][secondIndex]
-    ) {
-      return backtrack(firstIndex, secondIndex - 1);
-    }
-
-    return backtrack(firstIndex - 1, secondIndex);
-  }
-
-  const common = LCSLength(first, second);
-
-  return backtrack(first.length - 1, second.length - 1);
-}
